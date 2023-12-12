c85f139998a3195ad039e1896e22d17bb6a44a5c
youngunghan <hyulives@gmail.com>
2023-12-13 01:34:29 +0900

    import pytorch

--- mnist_classifier.py
+++ mnist_classifier.py
@@ -9,3 +9,9 @@
 # all other parts are almost same
 
 import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+from torchvision.datasets import MNIST
+from torchvision import transforms
