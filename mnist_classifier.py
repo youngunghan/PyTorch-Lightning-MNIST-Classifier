80f81134f82ece416a49dc77a038ab32fc143093
youngunghan <hyulives@gmail.com>
2023-12-12 22:24:42 +0900

    feature B

---
+++ mnist_classifier.py
@@ -0,0 +1,12 @@
+'''
+    Machine Learning Block Implementation Practice
+    with Pytorch Lightning
+
+    Author : Youngung Han (2023)
+'''
+
+# most of the case, you just change the component loading part
+# all other parts are almost same
+
+import torch
+addaf
